efb4c5aa7087a4222a829cfb7fab332e82c952a4
nyankosama <825138000@qq.com>
2015-03-21 15:13:50 +0800

    ch2 sequence interface

--- src/main/clojure/ch2/sequence-interface.clj
+++ src/main/clojure/ch2/sequence-interface.clj
@@ -1,16 +1,6 @@
-(defn ++ [n] (+ n 1))
-(defn -- [n] (- n 1))
-(defn += [n deta] (+ n deta))
-(defn -= [n deta] (- n deta))
-
-(defn square [n] (* n n))
-(defn cube [n] (* n n n))
-(defn average [v1 v2] (/ (+ v1 v2) 2))
-
-(defn gcd [a b]
-  (if (= b 0)
-    a
-    (gcd b (rem a b))))
+(ns
+  ^{:author nyankosama}
+  ch2.sequence-interface)
 
 ;map函数
 (defn my-map [proc items]
@@ -34,6 +24,16 @@
     init
     (op (first items) (my-accumulate op init (rest items)))))
 
-:test
+;对n个seq的accumulate
+(defn my-accumulate-n [op init items]
+  (if (empty? (first items))
+    nil
+    (cons (my-accumulate op init
+            (my-map first items))
+      (my-accumulate-n op init (my-map rest items)))))
+
+;test case
 (println (my-accumulate cons nil (my-map inc (my-filter even? (list 1 2 3 4 5)))))
 (println (my-accumulate max 0 (list 1 2 3 6 4)))
+(println (my-map first (list (list 1 2) (list 3 4))))
+(println (my-accumulate-n + 0 (list (list 1 2) (list 3 4))))
