d935ef70248be45f9d9165b9d80b32ebcfe00c7b
nyankosama <825138000@qq.com>
2015-03-21 12:00:22 +0800

    update ch2

--- src/main/clojure/ch2/subsets.clj
+++ src/main/clojure/ch2/subsets.clj
@@ -5,6 +5,8 @@
 ;假定集合为(1 2 3)，返回所有的子集的集合如(() (1) (2) (3) (1 2) (2 3) (1 3) (1 2 3))
 (defn subsets [s]
   (if (empty? s)
-    s
+    (list s) ;这里返回的一定要是一个最终返回集的中间结果集，集嵌套的集合
     (let [rst (subsets (rest s))]
-      (concat rst (map (fn [item] (cons (first s) item)) rst)))))
+      (concat rst (map #(cons (first s) %) rst)))))
+
+(println (subsets (list 1 2 3)))
